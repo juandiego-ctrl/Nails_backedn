58ec669f1f69e6822db9d673f2491bbdbc54be20
juandiego-ctrl <juandiego.cdp@gmail.com>
2026-01-15 10:41:21 -0500

    configuracion de la conexion

--- server.js
+++ server.js
@@ -8,10 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'))
+mongoose.connect(process.env.MONGO_URI).then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
 const appointmentRoutes = require('./routes/appointments');
