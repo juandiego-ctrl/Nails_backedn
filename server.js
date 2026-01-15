1267918fc38aa078c82b5e39c06ac8b4a097a926
juandiego-ctrl <juandiego.cdp@gmail.com>
2026-01-15 11:01:16 -0500

    contraseña

--- server.js
+++ server.js
@@ -14,5 +14,6 @@ mongoose.connect(process.env.MONGO_URI).then(() => console.log('MongoDB connecte
 const appointmentRoutes = require('./routes/appointments');
 app.use('/api/appointments', appointmentRoutes);
 
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
